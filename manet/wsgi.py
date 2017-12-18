407c9aba06c149f1fd8b000658ff024f14126b24
Hector <hmartineztobar@gmail.com>
2017-12-18 23:50:08 +0100

    additional files and changes for Heroku

--- manet/wsgi.py
+++ manet/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import 
+from django.core.wsgi import get_wsgi_application
 
 #heroku 
 from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manet.settings")
 
