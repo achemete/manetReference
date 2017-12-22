2d79b6d63551a777d78f20a0cd9cdfdea2af59f1
Hector <hmartineztobar@gmail.com>
2017-12-22 19:12:00 +0100

    Debug

--- backend/static/js/references.js
+++ backend/static/js/references.js
@@ -1,10 +1,8 @@
-console.log("me he cargado");
-$("#contact-btn").click(function(){
-	console.log("click contact from references");
+/*$("#contact-btn").click(function(){
 	$("#main").load("./contact.html");
 });	
 
 $("#explore-algs").click(function(){
-		console.log("click en explore algs");
 		$("#main").load({{% url 'b-algs' %}});
 });
+*/
