6d015c96764f3524cb9517fb77046fdf84429ec3
Hector <hmartineztobar@gmail.com>
2017-12-22 19:07:08 +0100

    BD TU/e

--- manet/settings.py
+++ manet/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'manet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # DATABASES = {
 # 	'default': {
@@ -114,16 +114,16 @@ WSGI_APPLICATION = 'manet.wsgi.application'
 #       }
 
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': 'manetrefpro',
-		'USER': 'manetdjango',
-		'PASSWORD': 'manetdjango',
-		'HOST': 'localhost',
-		'PORT': '',
-	}
-}
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+# 		'NAME': 'manetrefpro',
+# 		'USER': 'manetdjango',
+# 		'PASSWORD': 'manetdjango',
+# 		'HOST': 'localhost',
+# 		'PORT': '',
+# 	}
+# }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
