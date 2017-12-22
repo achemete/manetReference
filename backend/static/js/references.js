3a1d7ef6c348a2925dde19202b14301f2a027a98
Hector <hmartineztobar@gmail.com>
2017-12-22 19:17:05 +0100

    Url django

--- backend/static/js/references.js
+++ backend/static/js/references.js
@@ -1,8 +1,8 @@
-/*$("#contact-btn").click(function(){
-	$("#main").load("./contact.html");
+$("#contact-btn").click(function(){
+	$("#main").load("{% static '../backend/contact.html' %}");
 });	
 
-$("#explore-algs").click(function(){
-		$("#main").load({{% url 'b-algs' %}});
+/*$("#explore-algs").click(function(){
+		$("#main").load({{% static '../contact.html' %}});
 });
 */
